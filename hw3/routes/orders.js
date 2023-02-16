8fd147408a5ab92ddd385e15f5ae2b1fbba98534
Austen Furutani <furutani24@up.edu>
2023-02-15 21:42:43 -0800

    Updated to turn array into json

--- hw3/routes/orders.js
+++ hw3/routes/orders.js
@@ -1,22 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var cherryCount = new Object();
+cherryCount.topping = "Cherry";
+cherryCount.quantity = "2";
 
+var chocoCount = new Object();
+chocoCount.topping = "Chocolate";
+chocoCount.quantity = "3";
+
+var plainCount = new Object();
+plainCount.topping = "Plain";
+plainCount.quantity = "6";
+
+data = new Array();
+data.push(cherryCount);
+data.push(chocoCount);
+data.push(plainCount);
+var data = JSON.parse(JSON.stringify(data))
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    test={"data":[
-        {topping: "cherry", quantity: "2"},
-        {topping: "plain", quantity: "6"},
-        {topping: "chocolate", quantity: "3"}
-    ]}
-  res.send(test);
+  res.send(data);
 });
 router.post('/', function(req, res, next) {
-    test={"data":[
-        {topping: "Cherry", quantity: "2"},
-        {topping: "Chocolate", quantity: "3"},
-        {topping: "Plain", quantity: "6"}
-    ]}
-    res.json(test);
+    res.send(data);
 });
 
 module.exports = router;
